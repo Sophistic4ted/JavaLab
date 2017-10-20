266b0cc48e23119c05f1bc85dcfbc90283b12815
KamilKwiatkowski123 <i261914@mvrht.com>
2017-10-20 09:02:05 +0200

    Added Triangle drawing method

--- Lab3/3/Circle.java
+++ Lab3/3/Circle.java
@@ -1,10 +1,29 @@
 package myPackage;
 
+import java.util.Scanner;
+
 public class Circle extends Shape {
 
 	public void draw() {
-		// TODO Auto-generated method stub
-		
+	    char znak='*';
+	    int length;
+	    Scanner scan = new Scanner(System.in);;
+	    System.out.print("Wprowadz ramie trójkąta. ");
+	    length = scan.nextInt();
+	    for (int i = 0; i < length; i++) {//biezacy rzad	        
+	        for (int j = 0; j < length*2 ; j++) {	            
+	            if(i==length-1 && j <length) {
+	                System.out.print(" " + znak);
+	            }else if((j == length+1/2-i || j == length+1/2+i)&& i <length-1){
+	            	System.out.print(znak);
+	            }else  {
+	                System.out.print(" ");
+	            }
+	        }
+	        System.out.print(" ");
+	        System.out.print("\n");                         
+	    }
+	    scan.close();
 	}
 
 }
--- Lab3/3/Triangle.java
+++ Lab3/3/Triangle.java
@@ -11,10 +11,10 @@ public class Triangle extends Shape {
 	    System.out.print("Wprowadz ramie trójkąta. ");
 	    length = scan.nextInt();
 	    for (int i = 0; i < length; i++) {//biezacy rzad	        
-	        for (int j = 0; j < length ; j++) {	            
-	            if(i==length-1) {
+	        for (int j = 0; j < length*2 ; j++) {	            
+	            if(i==length-1 && j <length) {
 	                System.out.print(" " + znak);
-	            }else if(j == length+1/2-i || j == length+1/2+i){
+	            }else if((j == length+1/2-i || j == length+1/2+i)&& i <length-1){
 	            	System.out.print(znak);
 	            }else  {
 	                System.out.print(" ");
